2c032784d8691d50ea8b4bc0b2078c1473fc25ec
krishna soni <soni01krishna@gmail.com>
2024-01-27 14:18:35 +0530

    fixing in the user.filter in enroll router

--- Routes/enrollRouter.js
+++ Routes/enrollRouter.js
@@ -3,34 +3,44 @@ const enrollModel = require('../Models/enrollModel');
 
 const Router = express.Router()
 
-Router.post('/new-enroll', async (req,res)=>{
-    try{
-        const {userId,courseId} = req.body;
-        const user = await enrollModel.find({userId})
-        if(user && user.length >0){
-            const match =  user.filter(user => user.courseId === courseId)
-            if(match && match > 0){
+Router.post('/new-enroll', async (req, res) => {
+    try {
+        const { userId, courseId } = req.body;
+        const user = await enrollModel.find({ userId });
+
+        if (user && user.length > 0) {
+            
+            const match =  user.filter(enrollment =>  enrollment.courseId == courseId);
+            
+            if (match && match.length > 0) {
                 return res.status(200).json({
-                    status:true,
-                    message:"you are already register"
-                })
+                    status: true,
+                    message: "You are already registered",
+                });
+            }
+        } else {
+            const enroll = new enrollModel({
+                userId, courseId
+            });
+
+            const savedEnrollment = await enroll.save();
+
+            if (savedEnrollment) {
+                return res.status(200).json({
+                    status: true,
+                    message: "You are enrolled",
+                });
             }
         }
-        else{
-            const enroll = new enrollModel({
-                userId,courseId
-            })
-            const save = await enroll.save()
-            if(save) return res.status(200).json({
-                status:true,
-                message:"You are enroll"
-            })
-        }
-       
-    }catch(e){
-        console.log(e)
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({
+            status: false,
+            message: "Internal Server Error",
+        });
     }
-})
+});
+
 Router.get('/get-enroll/:userId', async (req,res)=>{
     try{
         const {userId} = req.params;
