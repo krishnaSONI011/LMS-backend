986f082f6852b9c97d46ae17bd0d5b6412509333
krishna soni <soni01krishna@gmail.com>
2024-04-16 22:55:51 +0530

    add update Route in Checked Router

--- Routes/watchedVideoRouter.js
+++ Routes/watchedVideoRouter.js
@@ -39,5 +39,22 @@ Router.post('/add-checked',async (req,res)=>{
         })
     }
 })
-
+Router.post('/update-checked',async (req,res)=>{
+    try{
+        const {id,userId,checked} = req.body
+        const result = await watchedVideos.findByIdAndUpdate(id,{checked})
+        const result2 = await watchedVideos.find({userId})
+        return res.status(200).json({
+            status:true,
+            result2
+        })
+       
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({
+            status:false,
+            message:"somthing went worng"
+        })
+    }
+})
 module.exports = Router
